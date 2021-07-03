a36cf10f541fd5f605f8ec04d9fd9a1d0988b081
Alexandre Silva <alexandre.silva@dsbsolucoes.com.br>
2021-07-03 11:21:38 -0300

    feat: adicionando uma tabela na home junto da paginação

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Container, Content, Table, Row } from './styles';
+import Pagination from '../../components/pagination';
 
 const Home: React.FC = () => {
-  return <></>;
+  return (
+    <Container>
+      <Content>
+        <div>
+          <h3>Últimas postagens</h3>
+        </div>
+        <Table>
+          <Row>
+            <div>teste</div>
+            <div>teste</div>
+          </Row>
+          <Row>
+            <div>teste</div>
+            <div>teste</div>
+          </Row>
+          <Row>
+            <div>teste</div>
+            <div>teste</div>
+          </Row>
+        </Table>
+        <div>
+          <div>
+            <p>Exibindo 9 postagens</p>
+          </div>
+          <Pagination current_page={1} maxPages={1} pages={5} />
+        </div>
+      </Content>
+    </Container>
+  );
 };
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,73 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #f0efef;
+  height: calc(100vh - 5rem);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 97%;
+  height: 92%;
+  border-radius: 3px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  > div:first-of-type {
+    padding: 2rem;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    > h3 {
+      color: #494949;
+    }
+  }
+
+  > div:last-of-type {
+    width: 99%;
+    border: 1px solid #efeeee;
+    border-top: 0;
+    padding: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+
+    > div {
+      margin-left: 1rem;
+      margin-right: 2rem;
+    }
+  }
+`;
+
+export const Table = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 99%;
+  border: 1px solid #efeeee;
+  min-height: calc(100% - 11rem);
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid #efeeee;
+  &:first-of-type {
+    background-color: #f0efef;
+    > div {
+      padding: 1rem 1.5rem;
+      color: #545454;
+      font-weight: bold;
+    }
+  }
+  &:last-of-type {
+    border-bottom: none;
+  }
+  > div {
+    padding: 1.5rem;
+    width: auto;
+    flex-grow: 1;
+  }
+`;
